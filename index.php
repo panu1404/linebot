b367810887e038b0ad4b36fa2d3c42de1af83c96
panu1404 <46158790+panu1404@users.noreply.github.com>
2020-01-23 17:24:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ if ( sizeof($request_array['events']) > 0 ) {
                if( $event['message']['type'] == 'text' ){
                    $text = $event['message']['text'];
                    if($text=='debug'){ $reply_message = json_encode($request_array);}else{
-                     $reply_message = 'ได้รับข้อความ (h '.$text.') แล้ว'; 
+                     $reply_message = 'ได้รับข้อความ (i '.$text.') แล้ว'; 
                        if($text=='hi'){
                          	 ////////////
 			       $userId = $event['source']['userId'];
@@ -32,7 +32,7 @@ if ( sizeof($request_array['events']) > 0 ) {
 			       	$profile_userid =getLINEProfile($LINEDatas);
 			      $reply_message =	getLINEProfile($LINEDatas);
 			       foreach ($profile_userid as $data_userid) {
-				        $reply_message = 'สวัสดีคุณ '.$profile_userid['message']['displayName'];
+				       // $reply_message = 'สวัสดีคุณ '.$profile_userid['message']['displayName'];
 			      		 }
 			       
 			      
@@ -109,7 +109,7 @@ function getLINEProfile($datas)
           $datasReturn['message'] = $response;
       }
    }
-   return $datasReturn;
+   return $response; ///  $datasReturn
 }
 /*
 {"events":[
