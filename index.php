485650d10e17ef6cefc25eb72188929a318a831a
panu1404 <46158790+panu1404@users.noreply.github.com>
2020-01-23 17:35:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -23,14 +23,14 @@ if ( sizeof($request_array['events']) > 0 ) {
                if( $event['message']['type'] == 'text' ){
                    $text = $event['message']['text'];
                    if($text=='debug'){ $reply_message = json_encode($request_array);}else{
-                     $reply_message = 'ได้รับข้อความ (j '.$text.') แล้ว'; 
+                     $reply_message = 'ได้รับข้อความ (k '.$text.') แล้ว'; 
                        if($text=='hi'){
                          	 ////////////
 			       $userId = $event['source']['userId'];
 			       		$LINEDatas['url'] = "https://api.line.me/v2/bot/profile/".$userId;
 			       		$LINEDatas['token']= $ACCESS_TOKEN;
 			       	$profile_userid =getLINEProfile($LINEDatas);
-			      $reply_message =	$profile_userid['result'];
+			      $reply_message =	$profile_userid['message']['userId'];
 			       foreach ($profile_userid as $data_userid) {
 				       // $reply_message = 'สวัสดีคุณ '.$profile_userid['message']['displayName'];
 			      		 }
